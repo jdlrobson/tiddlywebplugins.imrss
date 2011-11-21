f0fdd3c1072971ddce8537d8c36cca06f77299de
Jon Robson <jdlrobson@gmail.com>
2011-11-21 12:02:19 +0000

    FIRST COMMIT
    moved from svn.tiddlywiki.org

---
+++ setup.py
@@ -0,0 +1,24 @@
+# YOU NEED TO EDIT THESE
+AUTHOR = 'Jon Robson'
+AUTHOR_EMAIL = 'jdlrobson@gmail.com'
+NAME = 'tiddlywebplugins.imrss'
+DESCRIPTION = 'Allows you to import rss/atom feeds into tiddlers on your TiddlyWeb through a twanager command imrss. Also provides imrsswithtags which provides a mechanism to tag non-tagged rss feeds.'
+VERSION = '0.9.3'
+
+import os
+from setuptools import setup, find_packages
+# You should carefully review the below (install_requires especially).
+setup(
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    packages = find_packages(exclude=['test']),
+    platforms = 'Posix; MacOS X; Windows',
+    install_requires = ['setuptools', 'tiddlyweb','feedparser'],
+    zip_safe = False
+    )
---
+++ test/test_example.py
@@ -0,0 +1,45 @@
+from tiddlywebplugins import imrss
+from tiddlyweb.model.tiddler import Tiddler
+from tiddlyweb.model.bag import Bag
+import os
+from tiddlyweb import config
+from tiddlywebplugins.utils import get_store
+
+def setup():
+    store = get_store(config)
+    store.put(Bag("foo"))
+def teardown():
+    store = get_store(config)
+    store.delete(Bag("foo"))
+def test_savetiddlerstobag():
+    tid = Tiddler("test")
+    #imrss.savetiddlerstobag([tid], "foo")
+    
+def test_imrss():
+    imrss.init({})
+    tiddlers = imrss.tiddlers_from_rss("%s/test/example.xml"%os.getcwd())
+    assert len(tiddlers) is 2
+    
+    tiddlera= tiddlers[0]
+    title1 = tiddlera.title
+    text1 = u'<html><p><a href="http://example.org/2003/12/13/atom03">Atom-Powered Robots Run Amok</a><br/>The world is very scared.</p></html>'
+    assert  title1 == u"unique2"
+    assert tiddlera.text == text1
+    tiddlerb = tiddlers[1]
+    title2 = tiddlerb.title
+    assert title2 == u"http___google_co_uk_foo_html"
+    assert tiddlerb.modified == "20011213183002"
+
+    assert tiddlerb.fields['geo.long'] == u"0.429"
+    assert tiddlerb.fields['geo.lat'] == u"51.023"
+    
+def test_imrss_rss():
+  imrss.init({})
+  tiddlers = imrss.tiddlers_from_rss("%s/test/nolink.xml"%os.getcwd())
+  assert len(tiddlers) is 1
+  
+  tiddler= tiddlers[0]
+  assert tiddler.title == '00048598ec4da73c1e9bb'
+
+  assert tiddler.fields['geo.long'] == u"2.625732"
+  assert tiddler.fields['geo.lat'] == u"50.520412"
